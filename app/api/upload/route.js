dfca4a285db14d79d72b31ce84183e6327567b3f
AbdulHannan08128 <154576645+AbdulHannan08128@users.noreply.github.com>
2024-01-27 13:56:31 +0530

    a new commit

--- app/api/upload/route.js
+++ app/api/upload/route.js
@@ -10,7 +10,7 @@ export async function POST(request) {
 
     if (!file) {
       return NextResponse.json({ message: "No image found", success: false });
-    } 
+    } else {
       // Extract data from file
       const byteData = await file.arrayBuffer();
       const buffer = Buffer.from(byteData);
@@ -19,7 +19,7 @@ export async function POST(request) {
       const maxSize = 50 * 1024; // 50KB in bytes
       if (buffer.length > maxSize) {
         return NextResponse.json({ message: "!50kb", success: false });
-      } 
+      } else {
         // Convert image data to Base64
         const base64Image = buffer.toString("base64");
         // Create an appointment object
@@ -34,13 +34,13 @@ export async function POST(request) {
         await addAppointment(appointment, () => {
           console.log("Appointment Booked Successfully... Image Stored");
         });
-        return NextResponse.json({
+        NextResponse.json({
           message: "Appointment Booked Successfully",
           success: true,
         });
-      
-      
-    
+      }
+      NextResponse.json({ message: "Unsupported Error", success: false });
+    }
   } catch (error) {
     console.error("Error processing form data:", error);
     return NextResponse.json(
